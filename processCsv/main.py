10120a42d7cdefec952ab9fa1a9fe918818f0935
ChinoNya <cncapyj@gmail.com>
2021-03-27 12:23:52 +0800

    第七次作业

--- processCsv/main.py
+++ processCsv/main.py
@@ -1,50 +1,58 @@
+from python_console_menu import AbstractMenu, MenuItem
 import subprocess
+import sys
 
 
-def csvlook(args=None, input=None):
-    if input is not None:
-        p = subprocess.run('csvlook', input=input, capture_output=True)
+def run(cmd):
+    file = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    return file
+
+
+def student():
+    print('请输入要统计的班级名称')
+    classname = sys.stdin.readline().strip()
+    command = 'csvgrep -c 班级 -r ' + classname + ' data.csv | csvstat --count'
+    csv = run(command)
+    print(csv.stdout.read())
+
+
+def search():
+    print("请输入要查询的学生姓名：")
+    name = sys.stdin.readline().strip()
+    command = "csvgrep -c 姓名 -r " + name + " data.csv"
+    csv = run(command)
+    line = csv.stdout.readline().strip()
+    sys.stdout.flush()
+    line = csv.stdout.readline().strip()
+    if line is not '':
+        print("已找到")
+        return True
     else:
-        p = subprocess.run(' '.join(['csvlook', ' '.join(args)]), capture_output=True)
-    return p.stdout
+        print("未找到")
+        return False
 
 
-def csvcut(args=None, input=None):
-    if input is not None:
-        p = subprocess.run('csvcut', input=input, capture_output=True)
-    else:
-        p = subprocess.run(' '.join(['csvcut', ' '.join(args)]), capture_output=True)
-    return p.stdout
+def status():
+    print('请输入要统计的班级')
+    classname = sys.stdin.readline().strip()
+    print('请输入要统计的课程')
+    course = sys.stdin.readline().strip()
+    command = 'csvgrep -c 班级 -r ' + classname + ' data.csv | csvgrep -c 选课 -r ' + course
+    csv = run(command)
+    print(csv.stdout.read())
 
 
-def in2csv(file, input=None):
-    if input is not None:
-        p = subprocess.run('in2csv', input=input, capture_output=True)
-    else:
-        p = subprocess.run('in2csv ' + file, capture_output=True)
-    return p.stdout
+class MainMenu(AbstractMenu):
+    def __init__(self):
+        super().__init__("欢迎使用Csv集成操作系统")
+
+    def initialise(self):
+        self.add_menu_item(MenuItem(100, "退出").set_as_exit_option())
+        self.add_menu_item(MenuItem(101, "查询学生", lambda: search()))
+        self.add_menu_item(MenuItem(102, "统计班级学生数", lambda: student()))
+        self.add_menu_item(MenuItem(103, "查询班级中选课情况", lambda: status()))
 
 
-def csvstat(args=None, input=None):
-    if input is not None:
-        p = subprocess.run('csvstat', input=input, capture_output=True)
-    else:
-        p = subprocess.run(' '.join(['csvstat', ' '.join(args)]), capture_output=True)
-    return p.stdout
-
-
-def main():
-    file = 'data.csv'
-    cmd = ['-c ', '2', '3', '6']
-    args = [cmd[0] + ','.join(cmd[1:4]), file]
-    print(args)
-    # data = csvstat(input=csvcut(args=args))
-    data = csvcut(input=csvcut(args=args))
-    print(type(data))
-    data = str(data, encoding='utf-8')
-    print(data)
-    # print(data)
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    mainMenu = MainMenu()
+    mainMenu.display()
